1010fab37efa86d636df612113995788ed56d8a7
Eugene Rusinov <github@certalarm.ru>
2022-11-27 10:13:10 +0800

    Добавил классы для юнит-тестирования, установил xUnit в проекте AreaCalculatorLib.Tests.

---
+++ AreaCalculatorLib.Tests/CalcAreaInteractorTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using AreaCalculatorLib.Domain.UseCases;
+
+namespace AreaCalculatorLib.Tests
+{
+    public class CalcAreaInteractorTests
+    {
+
+    }
+}
