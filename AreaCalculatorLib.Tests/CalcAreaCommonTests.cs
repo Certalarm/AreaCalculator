c1049157930986efedb0dc1208586e5f9b24093b
Евгений <github@certalarm.ru>
2022-11-27 16:56:25 +0800

    Написал общие юнит-тесты для подсчета площадей.

---
+++ AreaCalculatorLib.Tests/CalcAreaCommonTests.cs
@@ -0,0 +1,62 @@
+﻿using AreaCalculatorLib.Domain.UseCases;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AreaCalculatorLib.Tests
+{
+    public class CalcAreaCommonTests
+    {
+        private readonly double[] values = new[] { 2.0, 3.0, 4.0 };
+        private readonly CalcAreaInteractor _sut;
+
+        #region .ctors
+        public CalcAreaCommonTests()
+        {
+            _sut = new CalcAreaInteractor();
+        }
+        #endregion
+
+        [Theory]
+        [MemberData(nameof(KnownFigureTypesAnyCase))]
+        public void Calc_area_when_known_figure_types_typed_any_case(string figureType)
+        {
+            var area = _sut.Execute(figureType, values);
+            var isAreaCalc = area != 0.0;
+
+            Assert.True(isAreaCalc);
+        }
+
+        public static List<object[]> KnownFigureTypesAnyCase()
+        {
+            return new List<object[]>
+            {   // format: figureType
+                new object[] { "circle" },             
+                new object[] { "CiRcLe" },  
+                new object[] { "CIRCLE" },  
+                new object[] { "triangle" },
+                new object[] { "TrIaNgLe" },
+                new object[] { "TRIANGLE" },
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(BadFigureTypes))]
+        public void Throw_exception_when_bad_figure_type(string figureType)
+        {
+            Assert.Throws<ArgumentException>(() => _sut.Execute(figureType, values));
+        }
+
+        public static List<object[]> BadFigureTypes()
+        {
+            return new List<object[]>
+            {   // format: figureType
+                new object[] { null! },
+                new object[] { string.Empty },
+                new object[] {" " },
+                new object[] { "ellipse" },
+                new object[] { "star" },
+            };
+        }
+    }
+}
--- AreaCalculatorLib.Tests/RightAngledTriangleTests.cs
+++ AreaCalculatorLib.Tests/RightAngledTriangleTests.cs
@@ -1,7 +1,11 @@
-﻿using AreaCalculatorLib.Domain.UseCases;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Xunit;
+using AreaCalculatorLib.Domain.UseCases;
+using AreaCalculatorLib.Domain.Entities.FigureEntity.Base;
 
 namespace AreaCalculatorLib.Tests
 {
